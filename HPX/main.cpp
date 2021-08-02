049df0bc1eabe6882ff275a429bafea7c5ecef45
Nikunj Gupta <nikunj.nkgpta@gmail.com>
2021-08-02 17:29:08 +0530

    Add cmake configuration for HPX implementation

---
+++ HPX/main.cpp
@@ -0,0 +1,14 @@
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/util.hpp>
+
+#include "../core/core.h"
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  App app(argc, argv);
+  app.display();
+
+  return 0;
+}
