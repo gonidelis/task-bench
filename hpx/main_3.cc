fe2f9debbe07c8338aad4adb4a90ff7894dbe3cd
nanmiaowu <nanmiaowu@gmail.com>
2021-09-23 21:12:36 -0500

    works for timestep 2

---
+++ hpx/main_3.cc
@@ -0,0 +1,270 @@
+//  Copyright (c)      2021 Nanmiao Wu
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include "../core/core.h"
+
+#include <cstdlib>
+#include <string> 
+#include <tuple>
+
+#include "hpx/hpx.hpp"
+#include "hpx/hpx_init.hpp"
+#include "hpx/include/actions.hpp"
+#include "hpx/local/chrono.hpp"
+#include "hpx/modules/collectives.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+constexpr char const* channel_communicator_name =
+  "hpx_block_task";
+
+///////////////////////////////////////////////////////////////////////////////
+int hpx_main(int argc, char *argv[]) 
+{    
+  // get number of localities and this locality
+  std::uint32_t num_localities = hpx::get_num_localities(hpx::launch::sync);
+  std::uint32_t this_locality = hpx::get_locality_id();
+
+  App app(argc, argv);
+  if (this_locality == 0) app.display();
+
+  std::vector<std::vector<char> > scratch;
+
+  for (auto graph : app.graphs) {
+    long first_point = this_locality * graph.max_width / num_localities;
+    long last_point = (this_locality + 1) * graph.max_width / num_localities - 1;
+    long n_points = last_point - first_point + 1;
+
+    size_t scratch_bytes = graph.scratch_bytes_per_task;
+    scratch.emplace_back(scratch_bytes * n_points);
+    TaskGraph::prepare_scratch(scratch.back().data(), scratch.back().size());
+  }
+  
+  double elapsed = 0.0;
+
+  // allocate channel communicator
+  auto comm = hpx::collectives::create_channel_communicator(hpx::launch::sync,
+      channel_communicator_name, hpx::collectives::num_sites_arg(num_localities),
+      hpx::collectives::this_site_arg(this_locality));
+      
+  using data_type = std::vector<char>;
+
+  for (int iter = 0; iter < 1; ++iter) {
+    
+    hpx::chrono::high_resolution_timer timer;
+
+    for (auto graph : app.graphs) {
+   
+      long first_point = this_locality * graph.max_width / num_localities;
+      long last_point = (this_locality + 1) * graph.max_width / num_localities - 1;
+      long n_points = last_point - first_point + 1;
+
+      size_t scratch_bytes = graph.scratch_bytes_per_task;
+      char *scratch_ptr = scratch[graph.graph_index].data();
+
+      std::vector<int> locality_by_point(graph.max_width);
+      for (int r = 0; r < num_localities; ++r) {
+        long r_first_point = r * graph.max_width / num_localities;
+        long r_last_point = (r + 1) * graph.max_width / num_localities - 1;
+        for (long p = r_first_point; p <= r_last_point; ++p) {
+          locality_by_point[p] = r;
+        }
+      }
+
+      long max_deps = 0;
+      for (long dset = 0; dset < graph.max_dependence_sets(); ++dset) {
+        for (long point = first_point; point <= last_point; ++point) {
+          long deps = 0;
+          for (auto interval : graph.dependencies(dset, point)) {
+            deps += interval.second - interval.first + 1;
+          }
+          max_deps = std::max(max_deps, deps);
+        }
+      }
+
+      // Create input and output buffers.
+      std::vector<std::vector<std::vector<char> > > inputs(n_points);
+      std::vector<std::vector<const char *> > input_ptr(n_points);
+      std::vector<std::vector<size_t> > input_bytes(n_points);
+      std::vector<long> n_inputs(n_points);
+      std::vector<std::vector<char> > outputs(n_points);
+      for (long point = first_point; point <= last_point; ++point) {
+        long point_index = point - first_point;
+
+        auto &point_inputs = inputs[point_index];
+        auto &point_input_ptr = input_ptr[point_index];
+        auto &point_input_bytes = input_bytes[point_index];
+
+        point_inputs.resize(max_deps);
+        point_input_ptr.resize(max_deps);
+        point_input_bytes.resize(max_deps);
+
+        for (long dep = 0; dep < max_deps; ++dep) {
+          point_inputs[dep].resize(graph.output_bytes_per_task);
+          point_input_ptr[dep] = point_inputs[dep].data();
+          point_input_bytes[dep] = point_inputs[dep].size();
+        }
+
+        auto &point_outputs = outputs[point_index];
+        point_outputs.resize(graph.output_bytes_per_task);
+      }
+
+      // Cache dependencies.
+      std::vector<std::vector<std::vector<std::pair<long, long> > > > dependencies(graph.max_dependence_sets());
+      std::vector<std::vector<std::vector<std::pair<long, long> > > > reverse_dependencies(graph.max_dependence_sets());
+      for (long dset = 0; dset < graph.max_dependence_sets(); ++dset) {
+        dependencies[dset].resize(n_points);
+        reverse_dependencies[dset].resize(n_points);
+
+        for (long point = first_point; point <= last_point; ++point) {
+          long point_index = point - first_point;
+
+          dependencies[dset][point_index] = graph.dependencies(dset, point);
+          reverse_dependencies[dset][point_index] = graph.reverse_dependencies(dset, point);
+        }
+      }
+
+      for (long timestep = 0; timestep < graph.timesteps; ++timestep) {
+        long offset = graph.offset_at_timestep(timestep);
+        long width = graph.width_at_timestep(timestep);
+
+        long last_offset = graph.offset_at_timestep(timestep-1);
+        long last_width = graph.width_at_timestep(timestep-1);
+
+        long dset = graph.dependence_set_at_timestep(timestep);
+        auto &deps = dependencies[dset];
+        auto &rev_deps = reverse_dependencies[dset];
+
+        for (long point = first_point; point <= last_point; ++point) {
+          long point_index = point - first_point;
+
+          auto &point_inputs = inputs[point_index];
+          auto &point_n_inputs = n_inputs[point_index];
+          auto &point_output = outputs[point_index];
+
+          auto &point_deps = deps[point_index];
+          auto &point_rev_deps = rev_deps[point_index];
+
+          /* Receive */
+          point_n_inputs = 0;
+          if (point >= offset && point < offset + width) {
+            for (auto interval : point_deps) {
+              for (long dep = interval.first; dep <= interval.second; ++dep) {
+                if (dep < last_offset || dep >= last_offset + last_width) {
+                  continue;
+                }
+
+                // Use shared memory for on-node data.
+                if (first_point <= dep && dep <= last_point) {
+                  auto &output = outputs[dep - first_point];
+                  point_inputs[point_n_inputs].assign(output.begin(), output.end());
+                } else {
+                  std::cout << "this_locality: " << this_locality << "point: "
+                                      << point << " at time: " << timestep
+                            << " begin the receive from deps, which is loc: "
+                            << locality_by_point[dep] << " \n";
+
+                  auto got_msg = hpx::collectives::get<data_type>(comm, 
+                      hpx::collectives::that_site_arg(locality_by_point[dep]));
+                  
+                  data_type rec_msg = got_msg.get();
+                  point_inputs[point_n_inputs].assign(rec_msg.begin(), rec_msg.end());
+
+                  std::cout << "this_locality: " << this_locality << " at time: " << timestep
+                            << " done the receive from deps, which is loc: "
+                            << locality_by_point[dep] << " \n";
+                }
+                point_n_inputs++;
+              }
+            }
+          }
+
+          // for time 0, first execute, then send
+          if (timestep == 0) {
+
+              auto &point_input_ptr = input_ptr[point_index];
+              auto &point_input_bytes = input_bytes[point_index];
+              graph.execute_point(timestep, point,
+                              point_output.data(), point_output.size(),
+                              point_input_ptr.data(), point_input_bytes.data(), point_n_inputs,
+                              scratch_ptr + scratch_bytes * point_index, scratch_bytes);              
+
+              for (auto interval : point_rev_deps) {
+              for (long dep = interval.first; dep <= interval.second; dep++) {
+                if (dep < offset || dep >= offset + width || (first_point <= dep && dep <= last_point)) {
+                  continue;
+                }
+                hpx::collectives::set(comm, 
+                    hpx::collectives::that_site_arg(locality_by_point[dep]), point_output).get();
+                
+              }
+            }
+          }
+          else {
+              // send for other times
+              if (point >= last_offset && point < last_offset + last_width) {
+                for (auto interval_send : point_rev_deps) {
+                  for (long dep_send = interval_send.first; dep_send <= interval_send.second; dep_send++) {
+                    if (dep_send < offset || dep_send >= offset + width || (first_point <= dep_send && dep_send <= last_point)) {
+                      continue;
+                    }
+                    std::cout << "this_locality: " << this_locality << " point: "
+                              << point << " at time: " << timestep
+                                << " begin the send to rev_deps \n";
+                    hpx::collectives::set(comm, 
+                        hpx::collectives::that_site_arg(locality_by_point[dep_send]), point_output).get();
+                    
+                  }
+                }
+              }
+          }
+        }
+
+        
+        for (long point = std::max(first_point, offset); point <= std::min(last_point, offset + width - 1); ++point) {
+          long point_index = point - first_point;
+          
+          auto &point_input_ptr = input_ptr[point_index];
+          auto &point_input_bytes = input_bytes[point_index];
+          auto &point_n_inputs = n_inputs[point_index];
+          auto &point_output = outputs[point_index];
+          graph.execute_point(timestep, point,
+                              point_output.data(), point_output.size(),
+                              point_input_ptr.data(), point_input_bytes.data(), point_n_inputs,
+                              scratch_ptr + scratch_bytes * point_index, scratch_bytes);
+        }
+
+        
+
+      }
+    }
+  
+    elapsed = timer.elapsed(); 
+  }
+
+
+
+  if (this_locality == 0) {
+    app.report_timing(elapsed);
+  }
+
+  return hpx::finalize();;
+}
+///////////////////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])
+{
+    // Initialize and run HPX, this example requires to run hpx_main on all
+    // localities
+    std::vector<std::string> const cfg = {
+        "hpx.run_hpx_main!=1",
+        "--hpx:ini=hpx.commandline.allow_unknown!=1",
+        "--hpx:ini=hpx.commandline.aliasing!=0"
+    };
+    hpx::init_params init_args;
+    init_args.cfg = cfg;  
+    
+    return hpx::init(argc, argv, init_args);
+}
